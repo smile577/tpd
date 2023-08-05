fbfd14e2bb3358ea236f1b2b5268876d0ae2ebad
smile577 <141505091+smile577@users.noreply.github.com>
2023-08-05 23:13:32 +0800

    Update model.py

--- model.py
+++ model.py
@@ -6,14 +6,6 @@ import torch
 import torch.nn as nn
 from transformers import RobertaModel
 
-'''from ..ops.misc import Conv2dNormActivation, MLP
-from ..transforms._presets import ImageClassification, InterpolationMode
-from ..utils import _log_api_usage_once
-from ._api import register_model, Weights, WeightsEnum
-from ._meta import _IMAGENET_CATEGORIES
-from ._utils import _ovewrite_named_param, handle_legacy_interface
-'''
-
 class ConvStemConfig(NamedTuple):
     out_channels: int
     kernel_size: int
